b9c13beccf38d7da6832b2b922c226c897557cb8
hoanghavi nguyen <hoanghavinguyen@gmail.com>
2020-06-02 19:41:46 +0700

    Add AspNetCore Identity

---
+++ EShop.Data/Entities/AppRole.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+
+namespace EShop.Data.Entities
+{
+    public class AppRole : IdentityRole<Guid>
+    {
+        public string Description { get; set; }
+    }
+}
