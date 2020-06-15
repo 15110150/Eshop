0e2aaf1c20ee8c928465ab9dd5ccf6947c93b8b7
hoanghavi nguyen <hoanghavinguyen@gmail.com>
2020-06-15 20:51:45 +0700

    Add product function

---
+++ Eshop.Service/Interfaces/IProductService.cs
@@ -0,0 +1,26 @@
+﻿using Eshop.ViewModel.Common;
+using Eshop.ViewModel.Product;
+using Eshop.ViewModel.ViewModel;
+using System.Threading.Tasks;
+
+namespace Eshop.Service.Interfaces
+{
+    public interface IProductService
+    {
+        Task<int> Create(ProductCreateRequest request);
+
+        Task<int> Update(ProductUpdateRequest request);
+
+        Task<int> Delete(int productId);
+
+        Task<ProductViewModel> GetById(int productId, string languageId);
+
+        Task<bool> UpdatePrice(int productId, decimal newPrice);
+
+        Task<bool> UpdateStock(int productId, int addedQuantity);
+
+        Task AddViewcount(int productId);
+
+        Task<PagedResult<ProductViewModel>> GetAllPaging(GetManageProductPagingRequest request);
+    }
+}
---
+++ Eshop.ViewModel/Common/PagingRequestBase.cs
@@ -0,0 +1,9 @@
+﻿namespace Eshop.ViewModel.Common
+{
+    public class PagingRequestBase
+    {
+        public int PageIndex { get; set; }
+
+        public int PageSize { get; set; }
+    }
+}
